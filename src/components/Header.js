6b4372112214ca8bf4677def5709e1edfe3b3425
Arthur Ghazaryan <newartroll11@gmail.com>
2021-07-24 22:10:11 +0400

    style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import SimpleDialogDemo from '../helper/OpenPage';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import SimpleDialogDemo from "../helper/OpenPage";
+import NestedList from "../helper/Navigation";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -27,13 +27,19 @@ export default function ButtonAppBar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Home
-          </Typography>
-          <Button color="inherit"><SimpleDialogDemo /></Button>
+          {<NestedList />} 
+          
+          <Button color="inherit">
+            <SimpleDialogDemo />
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
