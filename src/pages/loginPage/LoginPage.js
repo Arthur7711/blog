1017f07efebc6f914104fcb4475ab07bdfa44933
Arthur Ghazaryan <newartroll11@gmail.com>
2021-07-28 03:45:07 +0400

    validation form

--- src/pages/loginPage/LoginPage.js
+++ src/pages/loginPage/LoginPage.js
@@ -6,6 +6,7 @@ import React, { useState } from "react";
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
+  const [isVal, setIsVal] = useState("");
 
   const handleEmail = (e) => {
     setEmail(e.target.value);
@@ -14,29 +15,31 @@ export default function LoginPage() {
     setPass(e.target.value);
   };
   const register = () => {
-    console.log();
     if (
-      pass.length >= 6 &&
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
         email
-      ) === true
+      ) !== true
     ) {
-      console.log("welcome");
+      setIsVal("Wrong Email Address");
+    } else if (pass.length < 6) {
+      setIsVal("Password must be longer than 6 syboles");
     } else {
-      console.log("wrong");
+      localStorage.setItem("login", JSON.stringify([{ mail: email, password: pass }]));
+      setIsVal("");
     }
   };
   return (
     <div className=" flex justify-center align-center mt-40">
       <div className="border-2 rounded-md p-10">
         <h1 className="flex justify-center mt-4  text-3xl">Registration</h1>
-        <div className='mt-8'>
+        <p style={{ color: "red" }}>{isVal}</p>
+        <div className="mt-8">
           <Input placeholder="email" onChange={handleEmail} value={email} />
         </div>
-        <div className='mt-4'>
+        <div className="mt-4">
           <Input placeholder="password" onChange={handlePass} value={pass} />
         </div>
-        <div className='mt-4'>
+        <div className="mt-4">
           <Button onClick={register} color="primary">
             Register
           </Button>
